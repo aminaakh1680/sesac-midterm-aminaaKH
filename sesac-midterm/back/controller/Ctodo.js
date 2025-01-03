0bb8e769c1b5a769ac84fd177fd07c768f9298fa
aminaakh1680 <aminaakh1680@gmail.com>
2025-01-03 15:49:05 +0900

    [back-first]

---
+++ sesac-midterm/back/controller/Ctodo.js
@@ -0,0 +1,77 @@
+const { Todo } = require("../models/index");
+
+/* Todos 전체 목록 불러오기 */
+exports.readAll = async (req, res) => {
+  try {
+    const todos = await Todo.findAll();
+    console.log(todos);
+    res.send(todos);
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+/* Todo 한 개 불러오기 */
+exports.readOne = async (req, res) => {
+  try {
+    console.log(req.params);
+    const { todoId } = req.params;
+
+    const todo = await Todo.findOne({
+      where: { Id: todoId },
+    });
+    res.send(todo);
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+/* 새로운 Todo 생성 */
+//post todos
+exports.create = async (req, res) => {
+  try {
+    const newTodo = await Todo.create({
+      where: { title: title },
+      include: { done: done },
+    });
+    res.send(newTodo);
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+/* 기존 Todo 수정 */
+exports.update = async (req, res) => {
+  try {
+    const updatedTodo = await Todo.update(
+      {
+        done: req.body.done,
+      },
+      {
+        where: {
+          todoId: req.params.id,
+        },
+      }
+    );
+    res.send(updatedTodo);
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+/* 기존 Todo 삭제 */
+exports.delete = async (req, res) => {
+  try {
+    const { todoId } = req.params;
+    const isDeleted = await Todo.destroy({
+      where: {
+        id: todoId,
+      },
+    });
+    if (Boolean(isDeleted)) {
+      res.send("todo deleted successfully", todoId);
+    } else res.send(false);
+  } catch (Err) {
+    console.log("err", err);
+  }
+};
